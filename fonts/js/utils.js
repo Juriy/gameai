ee9d2055ae9c3865bd8d10051cb4e1bdd05643b9
Juriy Bura <juriy.bura@gmail.com>
2012-07-31 22:24:11 +0200

    Added the small fonts demo, to be used with the say bubbles

---
+++ fonts/js/utils.js
@@ -0,0 +1,50 @@
+
+/**
+ * Does the prototype-based inheritance as described in chapter 1.
+ * @param subConstructor the constructor function of the subclass
+ * @param superConstructor the constructor function of the superclass
+ */
+function extend(subConstructor, superConstructor) {
+    subConstructor.prototype = Object.create(superConstructor.prototype, {
+        constructor: {
+            value: subConstructor,
+            enumerable: false,
+            writable: true,
+            configurable: true
+        }
+    });
+}
+
+/**
+ * Checks if we're working with the touchscreen or with the
+ * regular desktop browser. Used to determine, what kind of events should we use:
+ * mouse events or touch events.
+ */
+function isTouchDevice() {
+    return ('ontouchstart' in document.documentElement);
+}
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+            || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
